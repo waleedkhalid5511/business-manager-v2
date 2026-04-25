3764f7210ceb22ed8f43ae6ffdfb0a5e40f90aec
waleedkhalid5511 <waleed.khalid5511@gmail.com>
2026-04-26 04:47:28 +0500

    Update Employees.jsx

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from './supabase'
+import { supabase } from '../supabase'
 import Employees from './pages/Employees'
 
 export default function App() {
