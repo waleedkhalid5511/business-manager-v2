0f9f1d35d1454a1d48a46bb05a2b78e74acc3705
waleedkhalid5511 <waleed.khalid5511@gmail.com>
2026-05-08 01:24:38 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const root = createRoot(document.getElementById('root'))
-root.render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
 )
-
-// Hide splash after app loads
-const hideSplash = () => {
-  const splash = document.getElementById('splash')
-  if (splash) {
-    splash.classList.add('hide')
-    setTimeout(() => splash.remove(), 500)
-  }
-}
-
-if (document.readyState === 'complete') {
-  setTimeout(hideSplash, 800)
-} else {
-  window.addEventListener('load', () => setTimeout(hideSplash, 800))
-}
